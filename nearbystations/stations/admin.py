d94b165a8a1e8433e6ef1bb5503ba47717d65d06
Shiloli-r <45619599+Shiloli-r@users.noreply.github.com>
2021-02-09 09:51:34 +0300

    Added Station Model, configured admin panel

--- nearbystations/stations/admin.py
+++ nearbystations/stations/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from django.contrib.gis.admin import OSMGeoAdmin
+from .models import Station
+
 
 # Register your models here.
+@admin.register(Station)
+class StationAdmin(OSMGeoAdmin):
+    list_display = ('name', 'location')
+
